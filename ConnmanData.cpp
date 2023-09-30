d5e7889845aef7382c5613d556d857580ed58ba0
hyaldiz <huseyinyaldizother@protonmail.com>
2023-09-30 16:27:58 +0300

    done: ConnmanData

--- ConnmanData.cpp
+++ ConnmanData.cpp
@@ -13,8 +13,8 @@ bool ConnmanData::array(QList<ConnmanData> &referanceList, const QDBusMessage &r
     referanceList.clear();
     qdbusArgument.beginArray();
 
-    while(!qdbusArgument.atEnd()) {
-
+    while(!qdbusArgument.atEnd())
+    {
         if(qdbusArgument.currentType() != QDBusArgument::StructureType)
             return false;
 
@@ -24,11 +24,34 @@ bool ConnmanData::array(QList<ConnmanData> &referanceList, const QDBusMessage &r
         qdbusArgument.endStructure();
         referanceList.append(data);
     }
+
     qdbusArgument.endArray();
+
     return true;
 }
 
-bool ConnmanData::map(QMap<QString, QVariant> &referanceMap, const QDBusMessage &referanceMessage)
+bool ConnmanData::map(ObjectMap_t &referanceMap, const QDBusMessage &referanceMessage)
 {
+    if(!referanceMessage.arguments().at(0).canConvert<QDBusArgument>())
+        return false;
 
+    const QDBusArgument& qdbusArgument = referanceMessage.arguments().at(0).value<QDBusArgument>();
+    if(qdbusArgument.currentType() != QDBusArgument::MapType)
+        return false;
+
+    referanceMap.clear();
+    qdbusArgument.beginMap();
+
+    while(!qdbusArgument.atEnd())
+    {
+        QString  key;
+        QVariant value;
+        qdbusArgument.beginMapEntry();
+        qdbusArgument >> key >> value;
+        qdbusArgument.endMapEntry();
+        referanceMap.insert(key,value);
+    }
+
+    qdbusArgument.endMap();
+    return true;
 }
